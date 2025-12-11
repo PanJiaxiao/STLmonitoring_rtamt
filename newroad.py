db215e96148a1e94b839e5c88b4ddfe3a0df2129
PanJiaxiao <18956673119@163.com>
2025-12-11 21:42:48 +0800

    Add files via upload

---
+++ newroad.py
@@ -0,0 +1,12 @@
+def cal_mindpoint(a,b):
+    c=(a+b)/2
+    print(c)
+
+if __name__ == "__main__":
+    a = -7.197
+    b = 5.0658
+
+    c = -10.987175
+    d = 11.063325
+    cal_mindpoint(a,c) #23.27625
+    cal_mindpoint(b, d)
